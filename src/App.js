c479ac412e34f7b17b7c3eee506d7a85beebbd05
oscardelalanza <oscardelalanza@gmail.com>
2022-02-14 19:46:48 -0800

    input styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { useForm } from './hooks/useForm'
 import Input from './components/Input'
 
 const App = () => {
-  const [form, handleChange] = useForm({ name: '' })
+  const [form, handleChange] = useForm({ name: '', lastName: '' })
   console.log(form)
   return (
     <form>
       <Input label="Name" value={form.name} name="name" onChange={handleChange} placeholder="Name"/>
+      <Input label="Last Name" value={form.lastName} name="lastName" onChange={handleChange} placeholder="Last name"/>
     </form>
   )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,8 @@
+import './Input.css'
+
 const Input = ({ label, ...rest}) => {
   return (
-    <div>
+    <div className="field">
       <label>{label}</label>
       <input {...rest}/>
     </div>
