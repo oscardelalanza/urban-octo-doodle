6884fa16eb673ea06b4dbe41fe2ff7400b8c9a90
oscardelalanza <oscardelalanza@gmail.com>
2022-02-14 21:44:28 -0800

    button styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useForm } from './hooks/useForm'
 import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
+import Button from './components/Button'
 
 const App = () => {
-  const [form, handleChange] = useForm({ name: '', lastName: '' })
+  const [form, handleChange] = useForm({ name: '', lastName: '', email: '' })
   console.log(form)
   return (
     <Container>
@@ -13,6 +14,8 @@ const App = () => {
           <form>
             <Input label="Name" value={form.name} name="name" onChange={handleChange} placeholder="Name"/>
             <Input label="Last Name" value={form.lastName} name="lastName" onChange={handleChange} placeholder="Last name"/>
+            <Input label="Email" value={form.email} name="email" onChange={handleChange} placeholder="email"/>
+            <Button>Sent</Button>
           </form>
         </div>
       </Card>
